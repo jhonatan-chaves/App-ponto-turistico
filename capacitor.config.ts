3afc21179a5be6400391dd5617d531971ed9d655
jhonatan-chaves <jhonatan.chaves001@gmail.com>
2023-05-12 12:09:13 -0300

    adicionando os locais para se visitar, na home.page

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'App-ponto-turistico',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
